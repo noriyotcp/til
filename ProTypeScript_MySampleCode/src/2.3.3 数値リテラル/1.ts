a27409004ff81e6072d00261afea876aa7ab97bb
noriyotcp <noriyo.akita@gmail.com>
2024-01-01 21:50:37 +0900

    2.3.3 数値リテラル/1.ts

---
+++ ProTypeScript_MySampleCode/src/2.3.3 数値リテラル/1.ts
@@ -0,0 +1,16 @@
+const binary = 0b1010;
+const octal = 0o755;
+const hexdecimal = 0xff;
+
+console.log(binary, octal, hexdecimal);
+
+// 指数表記
+const big = 1e8; // 1 * 10の8乗
+const small = 4e-5; // 4 * 10のマイナス5乗
+
+console.log(big, small);
+
+const million = 1_000_000;
+
+console.log(million);
+
