f9b6473ff1ba4e80ae937af1a153a923e3489f61
noriyotcp <noriyo.akita@gmail.com>
2024-01-01 21:56:57 +0900

    2.3.4 任意精度整数(BigInt)

---
+++ ProTypeScript_MySampleCode/src/2.3.4 任意精度整数(BigInt)/1.ts
@@ -0,0 +1,9 @@
+const bignum: bigint = (123n + 456n) * 2n; // Bigint リテラル、整数の後にnを書く
+console.log(bignum); // 1158n と表示される
+
+// BigInt 派生数のみしか扱えないので、除算の結果が小数になる場合は、小数点以下を切り捨てた整数になる
+const result = 5n / 2n; // 2n と表示される
+console.log(result);
+
+// Operator '+' cannot be applied to types '100n' and '50'.
+// const wrong = 100n + 50; // エラーになる
