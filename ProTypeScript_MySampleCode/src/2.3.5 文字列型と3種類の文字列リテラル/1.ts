8e25a8e631ceebe8a7e7ba0714cf0f338f27f967
noriyotcp <noriyo.akita@gmail.com>
2024-01-01 22:03:58 +0900

    2.3.5 文字列型と3種類の文字列リテラル

---
+++ ProTypeScript_MySampleCode/src/2.3.5 文字列型と3種類の文字列リテラル/1.ts
@@ -0,0 +1,16 @@
+const message: string = `Hello
+world!`;
+
+console.log(message);
+
+const str1: string = 'Hello';
+const str2: string = 'TypeScript';
+console.log(`${str1} ${str2}!`);
+
+console.log(`123 + 456 = ${123 + 456}`);
+
+// バックスラッシュを含めるには \\ と書く
+console.log("Hello \\world/");
+
+// Unicodeコードポイントを指定する
+console.log("Hello \u{796d} world!");
