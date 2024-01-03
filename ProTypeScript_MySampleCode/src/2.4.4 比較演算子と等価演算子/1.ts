bf91a6bde0d6c54d2d67698876af31c5027b1b2b
noriyotcp <noriyo.akita@gmail.com>
2024-01-03 14:04:36 +0900

    2.4.4 比較演算子と等価演算子

---
+++ ProTypeScript_MySampleCode/src/2.4.4 比較演算子と等価演算子/1.ts
@@ -0,0 +1,21 @@
+const left1 = -5, right1 = 0;
+console.log(left1 < right1);
+
+const left2 = 100n, right2 = 50n;
+console.log(left2 >= right2);
+
+const left3 = -10, right3 = 0;
+console.log(left3 > right3);
+
+const left4 = 12n, right4 = 8n;
+console.log(left4 <= right4);
+
+// true, a は o よりもコードポイントが小さいため
+console.log("apple" < "orange");
+
+const left: number = 1;
+const right: number = 2;
+
+console.log(left === right);
+console.log(left !== right);
+
