27e6021b1f6dd2ee90be7ff661be9c5928ba9081
noriyotcp <noriyo.akita@gmail.com>
2024-01-04 19:22:51 +0900

    2.4.5 論理演算子(1)真偽値の演算

---
+++ ProTypeScript_MySampleCode/src/2.4.5 論理演算子(1)真偽値の演算/1.ts
@@ -0,0 +1,11 @@
+const t = true, f = false;
+
+// 論理積演算子
+console.log(t && t);
+console.log(t && f);
+console.log(f && f);
+
+// 論理和演算子
+console.log(t || t);
+console.log(t || f);
+console.log(f || f);
---
+++ ProTypeScript_MySampleCode/src/2.4.5 論理演算子(1)真偽値の演算/2.ts
@@ -0,0 +1,18 @@
+import { createInterface } from "readline";
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("数値を入力してください:", (line) => {
+  const num = Number(line);
+  if (Number.isNaN(num)) {
+    console.log(`${num}は数値にしてください`);
+  } else if (0 <= num && num < 100) {
+    console.log(`${num}は0以上100未満です`);
+  } else {
+    console.log(`${num}は0以上100未満ではありません`);
+  }
+  rl.close();
+});
