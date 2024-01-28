5ef1d62386c9451a6d424ae0c101eefe607fc732
noriyotcp <noriyo.akita@gmail.com>
2024-01-28 09:46:46 +0900

    3.1.5 オブジェクトリテラル(3)スプレッド構文

---
+++ ProTypeScript_MySampleCode/src/3.1.5 オブジェクトリテラル(3)スプレッド構文/2.ts
@@ -0,0 +1,19 @@
+const obj1 = {
+  foo: 123,
+  bar: 456,
+  baz: 789
+};
+
+const obj2 = {
+  ...obj1,
+  foo: -9999,
+};
+
+// 'foo' is specified more than once, so this usage will be overwritten.
+// const obj2 = {
+//   foo: -9999,
+//   ...obj1,
+// };
+
+// { foo: -9999, bar: 456, baz: 789 }
+console.log(obj2);
