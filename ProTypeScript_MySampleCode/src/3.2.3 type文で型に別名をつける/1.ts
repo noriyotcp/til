fd3a6c673074e1fa14234d0cc512729ca2058ae7
noriyotcp <noriyo.akita@gmail.com>
2024-01-31 08:56:29 +0900

    3.2.3 type文で型に別名をつける

---
+++ ProTypeScript_MySampleCode/src/3.2.3 type文で型に別名をつける/1.ts
@@ -0,0 +1,17 @@
+// type文はどんな型にも別名をつけられる
+// あくまで型に別名をつけるだけである
+// type文は決して「新たに型を作って利用可能にする」ものではなく、「すでにある型に別名をつける」だけのものである
+
+// FooBarObj を宣言する前に使ってもOK
+const obj: FooBarObj = {
+  foo: 123,
+  bar: "Hello World!"
+}
+
+type FooBarObj = {
+  foo: number;
+  bar: string;
+}
+
+console.log(obj);
+
