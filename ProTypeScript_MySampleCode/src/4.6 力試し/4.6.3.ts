4710adeee4548d83e977e6a6b97a5b6da13ec10a
noriyotcp <noriyo.akita@gmail.com>
2024-03-18 22:52:27 +0900

    4.6.3 use two generics

--- ProTypeScript_MySampleCode/src/4.6 力試し/4.6.3.ts
+++ ProTypeScript_MySampleCode/src/4.6 力試し/4.6.3.ts
@@ -1,7 +1,7 @@
 // 4.6.3 コールバックの練習
 
-function map<T>(array: T[], callback: (x: T) => T): T[] {
-  const result: T[] = [];
+function map<T, U>(array: T[], callback: (x: T) => U): U[] {
+  const result: U[] = [];
 
   for (const i of array) {
     result.push(callback(i));
@@ -14,3 +14,8 @@ const data = [1, 1, 2, 3, 5, 8, 13];
 
 const result = map(data, (x) => x * 10);
 console.log(result);
+
+const data2 = [1, -3, -2, 8, 0, -1];
+
+const result2: boolean[] = map(data2, (x) => x >= 0);
+console.log(result2);
