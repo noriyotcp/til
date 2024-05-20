7b4740bb1ffa36839e4849b5889a6f0d1d7abaec
noriyotcp <noriyo.akita@gmail.com>
2024-05-21 02:40:39 +0900

    Update ProTypeScript_MySampleCode/src/6.5 as による型アサーション/column-29.ts

--- ProTypeScript_MySampleCode/src/6.5 as による型アサーション/column-29.ts
+++ ProTypeScript_MySampleCode/src/6.5 as による型アサーション/column-29.ts
@@ -17,12 +17,31 @@ function getOneUserName(user1?: Human, user2?: Human): string | undefined {
   // user2 がある場合に限られているはずだが、TypeScript はそれを理解していない
   // user2 の型は Human | undefined
   // return user2.name;
+}
+
+function showOneUserName(user1?: Human, user2?: Human): string | undefined {
+  if (user1 === undefined && user2 === undefined) {
+    return undefined;
+  }
+  if (user1 !== undefined) {
+    return user1.name;
+  }
+
   // ! をつける
-  // return user2!.name;
+  return user2!.name;
+
   // as で大体可能
   return (user2 as Human).name;
 }
 
-function getOneUserName2(user1?: Human, user2?: Human): string | undefined {
+console.log(showOneUserName({ name: "Taro", age: 25 }));
+console.log(showOneUserName(undefined, undefined));
+console.log(showOneUserName(undefined, { name: "Jiro", age: 30 }));
+
+function showOneUserName2(user1?: Human, user2?: Human): string | undefined {
   return user1?.name ?? user2?.name;
 }
+
+console.log(showOneUserName({ name: "Taro", age: 25 }));
+console.log(showOneUserName(undefined, { name: "Jiro", age: 30 }));
+console.log(showOneUserName(undefined, undefined));
