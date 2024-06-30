c58f49b90ea07416c24454225b427a2d0edb590b
noriyotcp <noriyo.akita@gmail.com>
2024-06-30 20:30:46 +0900

    chore: Add weight property to Phone class

--- CopilotSample/p003-dev/Phone.js
+++ CopilotSample/p003-dev/Phone.js
@@ -1,13 +1,17 @@
 class Phone {
-    constructor(modelName, price) {
+    constructor(modelName, price, weight) {
         this.modelName = modelName;
         this.price = price;
+        this.weight = weight;
     }
     getModelName() {
         return `modelName: ${this.modelName}`;
     }
     getPrice() {
         return `price: ${this.price}`;
+    }
+    getWeight() {
+        return `weight: ${this.weight}`;
     }
 }
 module.exports = Phone;
