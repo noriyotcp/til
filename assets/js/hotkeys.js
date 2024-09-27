227a60a04b4216843af58027e6b35c775328b237
noriyotcp <noriyo.akita@gmail.com>
2024-09-28 00:48:40 +0900

    Create hotkeys.js
    Detect OS platform and open a search form by pressing command+k or
    ctrl+k
    
    Do nothing if a search form has already opened

---
+++ assets/js/hotkeys.js
@@ -0,0 +1,60 @@
+function getOS() {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  const platform = window.navigator.platform.toLowerCase();
+
+  // Detect macOS
+  if (
+    userAgent.indexOf("macintosh") !== -1 ||
+    userAgent.indexOf("mac os x") !== -1 ||
+    platform.indexOf("mac") !== -1
+  ) {
+    return "macOS";
+  }
+
+  // Detect Windows
+  if (userAgent.indexOf("windows") !== -1 || platform.indexOf("win") !== -1) {
+    return "Windows";
+  }
+
+  // Detect Linux
+  if (userAgent.indexOf("linux") !== -1 || platform.indexOf("linux") !== -1) {
+    return "Linux";
+  }
+
+  // Detect iOS
+  if (/iphone|ipad|ipod/.test(userAgent)) {
+    return "iOS";
+  }
+
+  // Detect Android
+  if (/android/.test(userAgent)) {
+    return "Android";
+  }
+
+  return "Unknown";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const openSearchForm = () => {
+    const isSearchOpen = document
+      .querySelector(".search-content")
+      .classList.contains("is--visible");
+
+    // If the search form is already open, do nothing
+    if (isSearchOpen) {
+      return false;
+    }
+
+    document.querySelector(".search__toggle").click();
+    return false;
+  };
+
+  const os = getOS();
+
+  // Detect whether you are using macOS or not
+  if (os === "macOS") {
+    hotkeys("command+k", openSearchForm);
+  } else {
+    hotkeys("ctrl+k", openSearchForm);
+  }
+});
