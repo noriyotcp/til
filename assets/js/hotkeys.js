66c6ec0243bf57d607bfde1e723ea82f59bc1f2e
noriyotcp <noriyo.akita@gmail.com>
2024-09-28 11:50:26 +0900

    Allow to move items by pressing j

--- assets/js/hotkeys.js
+++ assets/js/hotkeys.js
@@ -35,13 +35,13 @@ function getOS() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  const searchIcon = document.querySelector('button.search__toggle');
+  const searchIcon = document.querySelector("button.search__toggle");
   if (!searchIcon) {
-    console.log('searchIcon is not found');
+    console.log("searchIcon is not found");
     return false;
-  };
-  searchIcon.setAttribute('tooltip', 'cmd/ctrl + k to open, esc to close');
-  searchIcon.setAttribute('tooltip-position', 'left');
+  }
+  searchIcon.setAttribute("tooltip", "cmd/ctrl + k to open, esc to close");
+  searchIcon.setAttribute("tooltip-position", "left");
 
   const openSearchForm = () => {
     const isSearchOpen = document
@@ -64,5 +64,64 @@ document.addEventListener("DOMContentLoaded", function () {
     hotkeys("command+k", openSearchForm);
   } else {
     hotkeys("ctrl+k", openSearchForm);
+  }
+
+  let focusedItemIndex = null;
+  // default index is null
+  // increment index by 1 when this function is called
+  // argument is the length of the list
+  const incrementIndex = (listLength) => {
+    // if current index is the last index of the list, does not increment
+    if (focusedItemIndex === null) {
+      focusedItemIndex = 0;
+    } else if (focusedItemIndex !== listLength - 1) {
+      focusedItemIndex++;
+    }
+  }
+
+  // setup
+  const entriesList = document.querySelector(".entries-list");
+  // If the entries list is not found, do nothing
+  if (!entriesList) {
+    return false;
+  }
+
+  const listItems = entriesList.querySelectorAll(".list__item");
+
+  const listItemLinks = entriesList.querySelectorAll(".list__item h2 > a");
+  console.log(listItemLinks);
+
+  listItemLinks.forEach((link, index) => {
+    link.addEventListener("focusout", (_event) => {
+      if (focusedItemIndex === index) {
+        focusedItemIndex = null;
+      }
+      console.log(`listItem link ${index} is unfocused`);
+      console.log(`focusedItemIndex is ${focusedItemIndex}`);
+    });
+  });
+
+  const focusListItem = () => {
+    // remove .item-focus class from all list items
+    listItems.forEach((linkItem) => {
+      linkItem.classList.remove("item-focus");
+    });
+
+    incrementIndex(listItems.length);
+    listItemLinks[focusedItemIndex].focus();
+    console.log(focusedItemIndex);
+
+    // listItems[focusedItemIndex].querySelector("a").focus({ focusVisible: true });
+    // // add .item-focus class to the first list item
+    // listItems[focusedItemIndex].classList.add("item-focus");
+
+    return false;
+  };
+
+  // Detect whether you are using macOS or not
+  if (os === "macOS") {
+    hotkeys("j", focusListItem);
+  } else {
+    hotkeys("j", focusListItem);
   }
 });
