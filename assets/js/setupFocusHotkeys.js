6613e2e98abd518910d55e9026a39a878a576c02
noriyotcp <noriyo.akita@gmail.com>
2024-10-04 01:20:02 +0900

    Reset focusedItemIndex by clicking `back to top` link

--- assets/js/setupFocusHotkeys.js
+++ assets/js/setupFocusHotkeys.js
@@ -1,9 +1,11 @@
-import { moveFocusToPreviousItem, moveFocusToNextItem, focusedItemIndex } from "./focusNavigation.js";
+import { moveFocusToPreviousItem, moveFocusToNextItem, focusedItemIndex, resetFocusedItemIndex } from "./focusNavigation.js";
 
 export const setupFocusHotkeys = () => {
   const listItemLinks = [];
   const taxonomy__indexes = document.querySelectorAll(".taxonomy__index");
   const entriesLists = document.querySelectorAll(".entries-list");
+
+  const backToTopLinks = document.querySelectorAll(".back-to-top");
 
   if (taxonomy__indexes.length > 0) {
     taxonomy__indexes.forEach((index) => {
@@ -25,7 +27,15 @@ export const setupFocusHotkeys = () => {
 
   listItemLinks.forEach((link, index) => {
     link.addEventListener("focusout", (_event) => {
+      // Even if the focus is out, the focusedItemIndex remains the same
       console.log(`index link ${index} is unfocused`);
+      console.log(`focusedItemIndex is ${focusedItemIndex}`);
+    });
+  });
+
+  backToTopLinks.forEach((link, index) => {
+    link.addEventListener("click", (_event) => {
+      resetFocusedItemIndex();
       console.log(`focusedItemIndex is ${focusedItemIndex}`);
     });
   });
