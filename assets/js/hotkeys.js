43a23fe02fd1cd373d68761933cca2dff6415e97
noriyotcp <noriyo.akita@gmail.com>
2024-10-06 10:49:14 +0900

    Add hotkeys to `Back to ...`

--- assets/js/hotkeys.js
+++ assets/js/hotkeys.js
@@ -22,5 +22,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Setup hotkeys popover hotkeys
     setupHotkeysPopoverHotkeys();
+
+    // Get all <a> elements with the class .back-to-top
+    const backToTopLinks = document.querySelectorAll("a.back-to-top");
+
+    backToTopLinks.forEach((link) => {
+      const href = link.getAttribute("href");
+
+      // Regular expression to get everything after #
+      const match = href.match(/#(.+)/);
+
+      if (match && match[1]) {
+        const content = match[1];
+
+        let hotkeyValue;
+        if (/^\d/.test(content)) {
+          // If it starts with a number, extract only the numbers and process them
+          const numbers = content.replace(/-/g, "");
+          hotkeyValue = "b " + numbers.split("").join(" ");
+        } else if (content === "page-title") {
+          // If it is page-title, assign `b t` as the hotkey
+          hotkeyValue = "b t";
+        }
+
+        link.setAttribute("data-hotkey", hotkeyValue);
+      }
+    });
   }
 });
