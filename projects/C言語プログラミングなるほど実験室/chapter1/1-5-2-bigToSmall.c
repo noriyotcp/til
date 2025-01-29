307da4455d872c4f73066cea7261e05d2c8bff6e
noriyotcp <noriyo.akita@gmail.com>
2025-01-29 23:02:55 +0900

    Create projects/C言語プログラミングなるほど実験室/chapter1/1-5-2-bigToSmall.c

---
+++ projects/C言語プログラミングなるほど実験室/chapter1/1-5-2-bigToSmall.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    long l; // signed 32-bit
+    short s; // signed 16-bit
+    char c; // signed 8-bit
+
+    l = 0x12345678;
+
+    // より小さなデータ型の変数に代入
+    s = l;
+    c = l;
+
+    printf("long = %x\n", l);
+    printf("short = %x\n", s);
+    printf("char = %x\n", c);
+
+    return 0;
+}
